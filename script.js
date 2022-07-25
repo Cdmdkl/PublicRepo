fa5d7ebd7c5b58ad30fcfb295be77cab9a98f12e
Cdmdkl <109253626+Cdmdkl@users.noreply.github.com>
2022-07-24 19:58:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,37 @@ window.scrollTo({
     behavior: "smooth"
 });
 });
+
+var gameStart = document.getElementById('RoShamBo')
+var options = ["R", "P", "S"];
+
+
+
+
+function rockPaperScissors() {
+    var userChoice = window.prompt("Choose R, P, or S!");
+    if (!userChoice) {
+        return;
+      }
+      
+    userChoice = userChoice.toUpperCase();    
+    var randomNumber = Math.floor(Math.random() * options.length);
+    var computerChoice = options[randomNumber];
+
+    window.alert("The Computer chose " + computerChoice)
+
+    if (
+        (userChoice === "R" && computerChoice === "S") ||
+        (userChoice === "S" && computerChoice === "P") ||
+        (userChoice === "P" && computerChoice === "R")
+        
+        ) 
+        { window.alert("You win!")
+    }
+    else if (userChoice == computerChoice) {
+        window.alert("Draw!")
+    } else window.alert("You Lose! :(")
+
+}
+
+gameStart.addEventListener("click", rockPaperScissors)
